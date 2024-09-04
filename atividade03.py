de7a70aeda36a1a0ae70a7b7922d4c4c15fac593
GustavoGaleno376 <gustabrffmano@gmail.com>
2024-09-04 20:29:13 -0300

    atividade 03 aréa do retangulo

---
+++ atividade03.py
@@ -0,0 +1,5 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+base=int(input("digite a base do retangulo"))
+altura=int(input("digite a altura do retangulo"))
+area=(base*altura)
+print(f"a area do seu retangulo é {area} ")
